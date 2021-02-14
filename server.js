6ed82bfe3dcbad7f5dad96b41e308c16c3193f9d
debo9210 <omololuadebowale1@gmail.com>
2021-02-14 02:36:58 -0500

    make app ready for deployment

--- server.js
+++ server.js
@@ -39,7 +39,7 @@ if (process.env.NODE_ENV === 'production') {
 // set up cors to allow us to accept requests from our client
 app.use(
   cors({
-    origin: url, // allow to server to accept request from different origin
+    origin: 'http://localhost:3000', // allow to server to accept request from different origin
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true, // allow session cookie from browser to pass through
   })
